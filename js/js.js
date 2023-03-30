ed51833831824d119783b8e98dd66542aabf70c2
Frontenddevkan <frontenddevkan@gmail.com>
2023-03-30 14:53:07 +0500

    feat: add adaptive design to header and welcome-section and add hamburger-menu and add js.js file

--- js/js.js
+++ js/js.js
@@ -1,9 +1,23 @@
 
-function sayHi(naming){
-    console.log(1.15 + 2.30);
-}
 
-sayHi();
-console.log(1.15 + 2.30);
-// null - обнуляет переменную
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.ul-nav'),
+  menuItem = document.querySelectorAll('.ul-nav'),
+  hamburger = document.querySelector('.hamburger');
+
+
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger_active');
+    menu.classList.toggle('nav-menu_active');
+});
+
+menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('nav-menu_active');
+    })
+  })
+})
+
+
 
